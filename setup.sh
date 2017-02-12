4debe9a4cec87508c97b1677512adf6cde847c05
Owain Lewis <owain@owainlewis.com>
2017-02-12 09:11:47 -0800

    Updates installer

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-function install_core_packages {
+install_core_packages () {
   apt-get update
 
   apt-get install -y \
@@ -10,31 +10,39 @@ function install_core_packages {
 	  nitrogen
 }
 
-function install_java 
+install_java () {
   apt-get install openjdk-8-jdk
 }
 
-function configure_git {
+configure_git () {
     git config --global user.email "owain@owainlewis.com"
     git config --global user.name "Owain Lewis"
 }
 
-function install_spacemacs {
-    rm -rf ~/.emacs.d && git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+install_spacemacs () {
+    rm -rf ~/.emacs.d
+    git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 }
 
-function install_zsh {
+install_zsh () {
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
-function install_haskell {
+install_haskell () {
     curl -sSL https://get.haskellstack.org/ | sh
 }
 
-function setup_ssh {
+install_lein () {
+    curl -O https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+    mv lein /usr/local/bin/lein
+    chmod a+x /usr/local/bin/lein
+    lein
+}
+
+setup_ssh () {
     ssh-keygen -t rsa -b 4096 -C "owain@owainlewis.com"
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
 }
 
-install_zsh
+install_lein
