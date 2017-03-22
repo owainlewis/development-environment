4ef0717496ad012b30d02564e49b41352ae7beaa
Owain Lewis <owain@owainlewis.com>
2017-03-22 20:25:10 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -80,3 +80,10 @@ setup_ssh () {
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa
 }
+
+install_sbt () {
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    sudo apt-get update
+    sudo apt-get install sbt
+}
