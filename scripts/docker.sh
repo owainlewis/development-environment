54768401adae8b4dfe91c4a4516119e0ed624001
Owain <owain@owainlewis.com>
2017-10-06 19:20:55 +0100

    Make executable, fix typo

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -6,11 +6,11 @@ apt install -y --no-install-recommends \
 
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 sudo add-apt-repository \
-            "deb https://apt.dockerproject.org/repo/ \
+	    "deb https://apt.dockerproject.org/repo/ \
    ubuntu-$(lsb_release -cs) \
    main"
 
 apt update
-apt-y install docker-engine    
-usermod -aG docker $USER    
+apt -y install docker-engine
+usermod -aG docker $USER
 sg docker -c "bash"
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 USER="Owain Lewis"
 EMAIL="owain@owainlewis.com"
 
-apt install -y git-core zsh emacs i3 feh
+apt install -y git zsh emacs i3
 
 git config --global user.email "${EMAIL}"
 git config --global user.name ${USER}
